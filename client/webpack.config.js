c36347893f2369c4422cfe4551a4288c209c0d8a
MariaFernandaMarroquin <MariaFernanda@MacBook-Pro-de-Martha.local>
2023-07-25 09:34:21 -0600

    service worker and manifest file plugins added, css and babel loader added in webpack congif

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Webpack Plugin
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+
+      //Service worker Plugin
+      new InjectManifest(),
+
+      //Mainfest file Plugin
+      new WebpackPwaManifest({
+        name: 'Text Editor',
+        short_name: 'T.E',
+        description: 'Text editor to store important data or code snippets',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('client/src/favicon.ico'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
