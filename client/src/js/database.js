35a67d4aa490424277de04a9c8029e5af2e62a75
MariaFernandaMarroquin <MariaFernanda@MacBook-Pro-de-Martha.local>
2023-07-26 10:52:16 -0600

    changes in database.js and install.js ()logic for installing the PWA)

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,45 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb implemented');
+
+  //Create connection to the db and the version
+  const jateDb = await openDB('jate', 1);
+
+  //Create transaction and specify db and data privileges
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  //Open desire object store
+  const store = tx.objectStore('jate');
+
+  //Put method to pass the content
+  const request = store.put({ id: id, data: content });
+
+  //Get confirmation of request
+  const result = await request;
+  console.log('Data saved to the database', result)
+}
+
+export const getDb = async () => {
+  console.error('getDb implemented');
+  //Create connection to the db and the version
+  const jateDb = await openDB('jate', 1);
+
+  //Create transaction and specify db and data privileges
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  //Open desire object store
+  const store = tx.objectStore('jate');
+
+  //getAll method to get data
+  const request = store.getAll();
+
+  //Get confirmation of request
+  const result = await request;
+  console.log('Data from database', result)
+  
+}
 
 initdb();
